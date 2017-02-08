faffc934d25202d6e05f0c9f70357a1d0455b94d
puyunfeng <910017207@qq.com>
2017-02-08 16:45:19 +0800

    点击位置的响应

--- app/src/main/java/com/example/pyf/myseattable/SeatTable.java
+++ app/src/main/java/com/example/pyf/myseattable/SeatTable.java
@@ -7,7 +7,11 @@ import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.widget.Toast;
 
 
 /**
@@ -19,7 +23,7 @@ public class SeatTable extends View {
     private int column = 5;
     Bitmap seatBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.seat_gray);//可选时座位的图片
     private Bitmap checkedSeatBitmap;
-    private Bitmap seatSoldBitmap=BitmapFactory.decodeResource(getResources(),R.mipmap.seat_sold);
+    private Bitmap seatSoldBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.seat_sold);
     private int verSpacing = 50;//座位垂直间距
     private int horSpacing = 50;//座位水平间距
     private int translateY = 50;//座位偏移的距离
@@ -28,8 +32,10 @@ public class SeatTable extends View {
     private static final int SEAT_TYPE_SOLD = 1;//座位已售
     private static final int SEAT_TYPE_AVAILABLE = 3;//座位可选
     private static final int SEAT_TYPE_SELECTED = 2;// 座位已经选中
+    private int seatWidth=seatBitmap.getWidth();//座位图片的宽度
+    private int seatHeight=seatBitmap.getHeight();//座位图片的高度
+    boolean isOnClick;
     private SeatChecker seatChecker;
-
 
 
     public void setSeatChecker(SeatChecker seatChecker) {
@@ -74,7 +80,7 @@ public class SeatTable extends View {
                 }
                 tempMatrix.setTranslate(left, top);
                 int seatType = getSeatType(i, j);
-                switch (seatType){
+                switch (seatType) {
                     case SEAT_TYPE_AVAILABLE:
                         canvas.drawBitmap(seatBitmap, tempMatrix, paint);
                         break;
@@ -94,13 +100,65 @@ public class SeatTable extends View {
     }
 
     private int getSeatType(int i, int j) {
-        if (seatChecker!=null) {
-            if(!seatChecker.isValidSeat(i,j)){
+        if (seatChecker != null) {
+            if (!seatChecker.isValidSeat(i, j)) {
                 return SEAT_TYPE_NOT_AVAILABLE;
-            }else if(seatChecker.isSold(i,j)){
+            } else if (seatChecker.isSold(i, j)) {
                 return SEAT_TYPE_SOLD;
-            };
+            }
+            ;
         }
         return SEAT_TYPE_AVAILABLE;
+    }
+
+    ScaleGestureDetector scaleGestureDetector = new ScaleGestureDetector(getContext(), new ScaleGestureDetector.OnScaleGestureListener() {
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector) {
+            return false;
+        }
+
+        @Override
+        public boolean onScaleBegin(ScaleGestureDetector scaleGestureDetector) {
+            return false;
+        }
+
+        @Override
+        public void onScaleEnd(ScaleGestureDetector scaleGestureDetector) {
+
+        }
+    });
+    GestureDetector gestureDetector = new GestureDetector(getContext(), new GestureDetector.SimpleOnGestureListener() {
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            isOnClick = true;
+            int x = (int) e.getX();
+            int y = (int) e.getY();
+            //最大的位置坐标
+            int posX=5*seatWidth+5*horSpacing+translateX+seatWidth;
+            int posY=5*seatHeight+5*verSpacing+translateY+seatHeight;
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < column; j++) {
+                    int tempX = (int) (j * seatWidth + j * horSpacing + translateX);
+                    int maxTempX = (int) (tempX + seatWidth);
+                    int tempY = (int) (i * seatHeight + i * verSpacing+translateY);
+                    int maxTempY = (int) (tempY + seatHeight);
+                    if (seatChecker != null && seatChecker.isValidSeat(i, j) && !seatChecker.isSold(i, j)) {
+                        if(x>=tempX&&x<=maxTempX&&y>=tempY&&y<=maxTempY){
+                            Toast.makeText(getContext(),"当前点击的位置是"+(i+1)+"行"+(j+1)+"列",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            }
+            return super.onSingleTapConfirmed(e);
+        }
+    });
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int y = (int) event.getY();
+        int x = (int) event.getX();
+        super.onTouchEvent(event);
+        gestureDetector.onTouchEvent(event);
+        return true;
     }
 }
